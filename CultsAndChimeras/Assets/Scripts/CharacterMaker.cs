7af7f0f8a30481de32eb53b0be04f266644331e8
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2019-04-05 20:48:57 -0400

    Rolling stats

---
+++ CultsAndChimeras/Assets/Scripts/CharacterMaker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class CharacterMaker : MonoBehaviour
+    {
+        public List<StatController> Stats;
+        public int UnspentPoints { private set; get; }
+
+        public void SpendPoint()
+        {
+            UnspentPoints -= 1;
+            //TODO: update UI
+        }
+
+        public void RollStats()
+        {
+            foreach (var stat in Stats)
+            {
+                var roll = RollXdY(3, 6);
+                stat.SetValue(roll);
+            }
+        }
+
+        public static int RollXdY(int x, int y)
+        {
+            int total = 0;
+            for (int i = 0; i < x; i++)
+            {
+                total += UnityEngine.Random.Range(1, y);
+            }
+            return total;
+        }
+    }
+
+   
+}
--- CultsAndChimeras/Assets/Scripts/StatController.cs
+++ CultsAndChimeras/Assets/Scripts/StatController.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StatController : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class StatController : MonoBehaviour
+    {
+        private int value = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [SerializeField] private Text valueText;
+
+        private void Awake()
+        {
+            
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Increase()
+        {
+            if(value >= 18)
+            {
+                return;
+            }
+
+
+        }
+
+        public void SetValue(int val)
+        {
+            value = val;
+            valueText.text = value.ToString();
+        }
+    }
 }
